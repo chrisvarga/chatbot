1f8e31d36894c9468aac299f697db15424b0be12
Nick Marble <nmarble@uccs.edu>
2017-02-01 21:59:02 +0000

    Added test file to test environment

---
+++ src/test.py
@@ -0,0 +1,18 @@
+from pattern.web import Twitter, plaintext
+from pattern.en import tag, parse
+
+twitter = Twitter(language='en')
+
+question = "How are you?"
+
+print parse(question).split()
+        
+for tweet in twitter.search('"A am feeling"', count=1, cached=False):
+    print plaintext(tweet.text)
+    
+question = "Do you like apple pie?"
+
+print parse(question).split()
+for tweet in twitter.search('"apple pie"', count=1, cached=False):
+    print plaintext(tweet.text)
+
