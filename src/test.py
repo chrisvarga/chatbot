c316403a2ec319071d09ccacf79bdf1cf4cc6620
Chris Varga <chris.j.varga@gmail.com>
2017-02-08 15:13:26 -0700

    example of basic classifier for Q/A

--- src/test.py
+++ src/test.py
@@ -1,8 +1,23 @@
 from pattern.web import Twitter, plaintext
 from pattern.en import tag, parse
 
+def classify_qa(tweet):
+    if "?" in tweet:
+        return "question"
+    else:
+        return "statement"
+
 twitter = Twitter(language='en')
 
+input = raw_input(">>> ")
+while input != "exit":
+    if classify_qa(input) == "question":
+        print("question")
+    else:
+        print("statement")
+    input = raw_input(">>> ")
+
+'''
 question = "How are you?"
 
 print parse(question).split()
@@ -15,4 +30,4 @@ question = "Do you like apple pie?"
 print parse(question).split()
 for tweet in twitter.search('"like apple pie"', count=1, cached=False):
     print plaintext(tweet.text)
-
+'''
