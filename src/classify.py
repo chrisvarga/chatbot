62a07d3dac2f4f8e7a5c6c7fdd325b63c69cbd9b
Chris Varga <chris.j.varga@gmail.com>
2017-03-19 17:06:05 -0600

    added functions to classify tweets into topics

--- src/classify.py
+++ src/classify.py
@@ -5,6 +5,7 @@
 #
 
 from pattern.db import Datasheet, pd
+from pattern.vector import Document, Model, KMEANS, count, COSINE
 
 def classify_question(tweet):
     if "?" in tweet or tweet.startswith("why") or tweet.startswith("how"):
@@ -26,3 +27,65 @@ def get_total_questions(file_name):
     print("percentage of questions: " +
             str(round(float(num_questions)/num_tweets,2)*100) + "%")
     return num_questions
+
+def basic_cluster():
+    table = Datasheet.load(pd("tweets.csv"))
+
+    questions = []
+    statements = []
+
+    for tweet in table:
+        if classify_question(tweet[1]):
+            questions.append(tweet[1])
+        else:
+            statements.append(tweet[1])
+
+    return questions, statements
+
+def kmeans_cluster():
+    table = Datasheet.load(pd("tweets.csv"))
+
+    i = 0
+    docs = ();
+    for tweet in table:
+        if i == 100:
+            break
+        d = Document(tweet[1], name=tweet[1])
+        docs = docs + (d,)
+        i = i + 1
+
+    m = Model(docs)
+    clusters = m.cluster(method=KMEANS, k=10, iterations=10, distance=COSINE)
+    for c in clusters:
+        print "cluster"
+        for tweet in c:
+            print tweet.name
+        print
+
+def latent_semantic_analysis():
+
+    table = Datasheet.load(pd("tweets.csv"))
+    i = 0
+
+    docs = ();
+    for tweet in table:
+        if i == 10:
+            break
+        d = Document(tweet[1], name=tweet[1])
+        docs = docs + (d,)
+        i = i + 1
+
+    m = Model(docs)
+    m.reduce(5)
+
+
+    for d in m.documents:
+        print
+        print "tweet: " + d.name
+        for concept, w1 in m.lsa.vectors[d.id].items():
+            for feature, w2 in m.lsa.concepts[concept].items():
+                if w1 != 0 and w2 != 0:
+                    print (feature, w1 * w2)
+
+# print each tweet with a probability of it being in each topic
+latent_semantic_analysis()
