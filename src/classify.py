d22d18cdcde9d3bdfcd410f58ea74fe61974149c
Chris Varga <chris.j.varga@gmail.com>
2017-03-20 09:24:35 -0600

    modified similarity.py to use kmeans, then cos sim

--- src/classify.py
+++ src/classify.py
@@ -5,7 +5,8 @@
 #
 
 from pattern.db import Datasheet, pd
-from pattern.vector import Document, Model, KMEANS, count, COSINE
+from pattern.vector import Document, Model, KMEANS, count, COSINE, HIERARCHICAL
+from random import choice
 
 def classify_question(tweet):
     if "?" in tweet or tweet.startswith("why") or tweet.startswith("how"):
@@ -34,7 +35,11 @@ def basic_cluster():
     questions = []
     statements = []
 
+    i = 0
     for tweet in table:
+        if i == 5:
+            break
+        i = i + 1
         if classify_question(tweet[1]):
             questions.append(tweet[1])
         else:
@@ -42,7 +47,7 @@ def basic_cluster():
 
     return questions, statements
 
-def kmeans_cluster():
+def kmeans_cluster(inp):
     table = Datasheet.load(pd("tweets.csv"))
 
     i = 0
@@ -54,30 +59,57 @@ def kmeans_cluster():
         docs = docs + (d,)
         i = i + 1
 
+    # cluster input with similar tweets
+    x = Document(inp, name=inp)
+    docs = docs + (x,)
     m = Model(docs)
     clusters = m.cluster(method=KMEANS, k=10, iterations=10, distance=COSINE)
-    for c in clusters:
-        print "cluster"
-        for tweet in c:
-            print tweet.name
-        print
+    #clusters = m.cluster(method=HIERARCHICAL, k=1, iterations=1000, distance=COSINE)
 
-def latent_semantic_analysis():
+    responses = []
+    for c in clusters:
+        for tweet in c:
+            if tweet.name == inp:
+                for tweet in c:
+                    if tweet.name != inp:
+                        responses.append(tweet.name)
+    return responses
+
+def rank_response(responses, inp):
+    best = []
+    for r in responses:
+        x = num_common_nouns(r, inp)
+        best.append({x : r})
+    return sorted(best)
+
+def num_common_nouns(r, inp):
+    pass
+    #words = parse(r)
+
+def latent_semantic_analysis(inp):
 
     table = Datasheet.load(pd("tweets.csv"))
-    i = 0
 
+    i = 0
     docs = ();
     for tweet in table:
-        if i == 10:
+        if i == 5:
             break
-        d = Document(tweet[1], name=tweet[1])
+        d = Document(tweet, name=tweet)
         docs = docs + (d,)
         i = i + 1
 
+    # add input to data
+    x = Document(inp, name=inp)
+    docs = docs + (x,)
     m = Model(docs)
-    m.reduce(5)
+    m.reduce(2)
 
+    print "the topics are:"
+    topics = []
+    for topic in m.lsa.concepts[0].keys():
+        topics.append(topic)
+    print sorted(topics)
 
     for d in m.documents:
         print
@@ -87,5 +119,10 @@ def latent_semantic_analysis():
                 if w1 != 0 and w2 != 0:
                     print (feature, w1 * w2)
 
-# print each tweet with a probability of it being in each topic
-latent_semantic_analysis()
+inp = raw_input("$ ")
+while inp != 'exit':
+    responses = kmeans_cluster(inp)
+    print choice(responses)
+    #latent_semantic_analysis(inp)
+    inp = raw_input("$ ")
+
--- src/similarity.py
+++ src/similarity.py
@@ -1,7 +1,35 @@
 import re, math
 from collections import Counter
-from pattern.vector import Document, Model, TFIDF
+from pattern.vector import Document, Model, TFIDF, KMEANS, count, COSINE, HIERARCHICAL
 from pattern.db import Datasheet, pd
+
+def kmeans_cluster(inp):
+    table = Datasheet.load(pd("tweets.csv"))
+
+    i = 0
+    docs = ();
+    for tweet in table:
+        if i == 100:
+            break
+        d = Document(tweet[1], name=tweet[1])
+        docs = docs + (d,)
+        i = i + 1
+
+    # cluster input with similar tweets
+    x = Document(inp, name=inp)
+    docs = docs + (x,)
+    m = Model(docs)
+    clusters = m.cluster(method=KMEANS, k=10, iterations=10, distance=COSINE)
+    #clusters = m.cluster(method=HIERARCHICAL, k=1, iterations=1000, distance=COSINE)
+
+    responses = []
+    for c in clusters:
+        for tweet in c:
+            if tweet.name == inp:
+                for tweet in c:
+                    if tweet.name != inp:
+                        responses.append(tweet.name)
+    return responses
 
 def rank_response(tweets, inp):
     responses = []
@@ -54,6 +82,7 @@ for tweet in table:
 
 inp = raw_input("$ ")
 while inp != 'exit':
+    tweets = kmeans_cluster(inp)
     print rank_response(tweets, inp)[0].values()[0]
     inp = raw_input("$ ")
 
