88205661b3beda81eeb591e06fdb332f116fc1a6
Nick Marble <nmarble@uccs.edu>
2017-04-02 17:22:43 +0000

    Attempting to add main.py

---
+++ src/all_imports.py
@@ -0,0 +1,6 @@
+#Place all names of .py in here for easy import
+#Use from all_imports import * in every file
+import db
+import classify
+import main
+
---
+++ src/main.py
@@ -0,0 +1,36 @@
+from all_imports import *
+
+#Common values for easy use
+databaseFileName = "tweets.csv"
+groupsize = 20
+maxRounds = 2
+
+
+db.update_db(databaseFileName, "", 1000, False)
+
+#This Whole loop thing needs to be fixed. Other options wont take the first input
+inp = None    
+while inp != 'exit':
+    #Get input
+    inp = raw_input("$ ")
+    
+    
+    #Get all of inputs subjects
+    subjects = classify.get_input_subjects(inp)
+    
+    
+    #Get tweets until group size is reached, stops if rounds get to high to
+    #prevent over requesting from twitter
+    rounds = 0
+    similarTweets = []
+    while len(similarTweets) < groupsize:
+        #This is where it gets the similar tweets from the DB
+        similarTweets = classify.get_tweets_with_subject(subjects, databaseFileName, groupsize)
+        if rounds > maxRounds:
+            break
+        db.update_db(databaseFileName, "", 1000, False) 
+        rounds = rounds + 1
+    
+    #Print the best result from all that was found.
+    print (classify.get_tfidf(inp, similarTweets))
+    
